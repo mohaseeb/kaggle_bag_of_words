d9f85d97c749926c7b30e8827586cbef68efd87d
haseeb <mohamedaziz52@gmail.com>
2015-06-03 00:06:04 +0200

    training data ready

--- code/predict_sentiment.py
+++ code/predict_sentiment.py
@@ -3,12 +3,22 @@ import pandas as pn
 import re
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 
 TRAIN_FILE = "../data/labeledTrainData.tsv"
 
 
 def get_train_data(train_file):
     return pn.read_csv(train_file, header=0, delimiter="\t", quoting=3)
+
+
+def clean_review(review):
+    text = BeautifulSoup(review).get_text()
+    letters_only = re.sub("[^a-zA-Z]", " ", text)
+    words = letters_only.lower().split()
+    stops = stopwords.words("english") #faster
+    words = [w for w in words if w not in stops]
+    return " ".join(words)
 
 
 def clean_date(data):
@@ -21,15 +31,15 @@ def clean_date(data):
     return cleaned_data
 
 
-def clean_review(review):
-    text = BeautifulSoup(review).get_text()
-    letters_only = re.sub("[^a-zA-Z]", " ", text)
-    words = letters_only.lower().split()
-    stops = stopwords.words("english") #faster
-    words = [w for w in words if w not in stops]
-    return " ".join(words)
+def train(training_file):
+    data = get_train_data(training_file)
+    cleaned_data = clean_date(data)
+    vectorizer = CountVectorizer(analyzer="word", tokenizer=None, preprocessor=None, stop_words=None, \
+                                 max_features=5000)
+    training_data = vectorizer.fit_transform(cleaned_data)
+    training_data = training_data.toarray()
+    print training_data.shape
 
 
 if __name__ == '__main__':
-    train_data = get_train_data(TRAIN_FILE)
-    print len(clean_date(train_data))
+    train(TRAIN_FILE)
